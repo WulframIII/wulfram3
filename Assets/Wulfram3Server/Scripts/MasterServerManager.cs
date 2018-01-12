0d94b9cf68ec2ae409fc67f5b75bd7da0d1bae33
Daniel Matthews <knight1219@gmail.com>
2018-01-11 22:18:51 -0500

    Added folders for Server related code
    Added some folders that we can store server-only related stuff in.

---
+++ Assets/Wulfram3Server/Scripts/MasterServerManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://gamedev.stackexchange.com/questions/128410/how-to-run-unity-in-headless-mode
+// https://answers.unity.com/questions/353653/can-a-unity-game-be-started-over-ssh.html
+public class MasterServerManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
