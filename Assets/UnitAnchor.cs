3c33d144a997f6aee9696ead5d9a1502a8f58ab1
Cheebsta <35013256+Cheebsta@users.noreply.github.com>
2018-01-14 20:47:08 -0500

    Flying Missile Launcher Fix

--- Assets/UnitAnchor.cs
+++ Assets/UnitAnchor.cs
@@ -9,7 +9,7 @@ namespace Com.Wulfram3 {
         private Vector3 anchorPosition;
         private float anchorStrength;
         private float distanceFromAnchor;
-        private float turretHeight;
+        private float unitHeight; // used for gun and flak turrets
         private Rigidbody myRigidbody;
         private Unit myUnit;
         private bool isAnchored = false;
@@ -17,7 +17,8 @@ namespace Com.Wulfram3 {
         private float diffZ;
         private float xVelocitySmoothing = 0.0f;
         private float zVelocitySmoothing = 0.0f;
-
+        private float rotationSmoothing = 0.0f;
+        private Quaternion referenceRotation;
         // Use this for initialization
         void Start() {
             myRigidbody = GetComponent<Rigidbody>();
@@ -30,20 +31,21 @@ namespace Com.Wulfram3 {
             RaycastHit groundInfo;
             Physics.Raycast(groundFinder, out groundInfo);
             anchorPosition = groundInfo.point;
+            //Debug.Log(transform.name + " anchoring to " + anchorPosition);
             myUnit = GetComponent<Unit>();
             if (myUnit != null)
             {
                 if (myUnit.unitType == UnitType.FlakTurret) {
-                    turretHeight = 6f;
-                    anchorStrength = 3f;
+                    unitHeight = 6f;
+                    anchorStrength = .1f;
                 }
                 if (myUnit.unitType == UnitType.GunTurret) {
-                    turretHeight = 4f;
-                    anchorStrength = 0.1f;
+                    unitHeight = 4f;
+                    anchorStrength = 1f;
                 }
                 if (myUnit.unitType == UnitType.MissleLauncher) {
-                    turretHeight = 0.05f;
-                    anchorStrength = 3f;
+                    unitHeight = 0.05f;
+                    anchorStrength = .05f;
                 }
             }
 
@@ -73,23 +75,34 @@ namespace Com.Wulfram3 {
 
         private void FixedUpdate()
         {
-            Ray ray = new Ray(CenteredLowestPoint(), -Vector3.up);
-            Ray checkRay = new Ray(transform.position, -Vector3.up); // This double check helps make sure the tank doesn't get stuck in the ground
-            RaycastHit hit;
-            RaycastHit groundCheck;
-            if (Physics.Raycast(ray, out hit, turretHeight) || (Physics.Raycast(checkRay, out groundCheck, turretHeight) && groundCheck.distance != 0 && hit.distance == 0))
+            if (unitHeight > 0.05f)
             {
-                if (hit.distance < turretHeight)
+                Ray ray = new Ray(CenteredLowestPoint(), -Vector3.up);
+                Ray checkRay = new Ray(transform.position, -Vector3.up); // This double check helps make sure the unit doesn't get stuck in the ground
+                RaycastHit hit;
+                RaycastHit groundCheck;
+                if (Physics.Raycast(ray, out hit, unitHeight) || (Physics.Raycast(checkRay, out groundCheck, unitHeight) && groundCheck.distance != 0 && hit.distance == 0))
                 {
-                    float ftcGravity = Physics.gravity.y * myRigidbody.mass;
-                    float ftcVelocity = myRigidbody.velocity.y * myRigidbody.mass;
-                    float multi = (turretHeight - hit.distance) / turretHeight;
-                    float force = (ftcGravity + ftcVelocity) * (multi * myRigidbody.mass);
-                    myRigidbody.AddForce(new Vector3(0f, -force, 0f));
+                    if (hit.distance < unitHeight)
+                    {
+                        float ftcGravity = Physics.gravity.y * myRigidbody.mass;
+                        float ftcVelocity = myRigidbody.velocity.y * myRigidbody.mass;
+                        float multi = (unitHeight - hit.distance) / unitHeight;
+                        float force = (ftcGravity + ftcVelocity) * (multi * myRigidbody.mass);
+                        myRigidbody.AddForce(new Vector3(0f, -force, 0f));
+                    }
                 }
             }
-            float newX = Mathf.SmoothDamp(transform.position.x, anchorPosition.x, ref xVelocitySmoothing, anchorStrength);
-            float newZ = Mathf.SmoothDamp(transform.position.z, anchorPosition.z, ref zVelocitySmoothing, anchorStrength);
+            distanceFromAnchor = Vector3.Distance(transform.position, anchorPosition);
+            if (myUnit.unitType == UnitType.GunTurret && myUnit.unitTeam == PunTeams.Team.blue)
+            {
+                //Debug.Log("Current Position: " + transform.position);
+                //Debug.Log("Anchor  Position: " + anchorPosition);
+                //Debug.Log("Distance From Anchor: " + distanceFromAnchor);
+
+            }
+            float newX = Mathf.SmoothDamp(transform.position.x, anchorPosition.x, ref xVelocitySmoothing, distanceFromAnchor / anchorStrength);
+            float newZ = Mathf.SmoothDamp(transform.position.z, anchorPosition.z, ref zVelocitySmoothing, distanceFromAnchor / anchorStrength);
             transform.position = new Vector3(newX, transform.position.y, newZ);
 
         }
