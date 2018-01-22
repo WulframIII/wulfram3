baee44d71dbdb19cd11902a1d643002fe953750e
Daniel Matthews <knight1219@gmail.com>
2018-01-21 22:50:02 -0500

    WIP for lobby

---
+++ Assets/Scenes/Lobby.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lobby : MonoBehaviour {
+
+    public GameObject loadingPanel;
+
+    public GameObject background;
+    private UniGifImage gif;
+
+
+    // Use this for initialization
+    void Start () {
+        this.gif = background.GetComponent<UniGifImage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.gif.nowState == UniGifImage.State.Playing)
+        {
+            loadingPanel.SetActive(false);
+        }
+        else
+        {
+            loadingPanel.SetActive(true);
+        }
+	}
+}
